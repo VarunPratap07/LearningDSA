c911554fb14d3364ab0dbf79a52883f6d720f17e
VarunPratap07 <varunpratapsinghshishodia@gmail.com>
2026-06-10 11:34:58 +0530

    Commit.

--- TheBasics/Connditional_Statements/If_elseif_else.cpp
+++ TheBasics/Connditional_Statements/If_elseif_else.cpp
@@ -2,33 +2,30 @@
 using namespace std;
 
 int main() {
-    // Declare and initialize marks variable
     int marks ;
     std::cout << "Please enter the value of marks = " ;
     std::cin >> marks ;
     std::cout << "The score is "<< marks << ".\n" ;
-    // Check grade conditions using if-else ladder
     if (marks < 25) {
-        cout << "Your Grade is F." << endl;  // Less than 25 is Grade F
+        cout << "Your Grade is F." << endl;  
     } 
     else if (marks >= 25 && marks <= 44) {
-        cout << "Your Grade is E." << endl;  // Between 25 and 44 is Grade E
+        cout << "Your Grade is E." << endl;  
     } 
     else if (marks >= 45 && marks <= 49) {
-        cout << "Your Grade is D." << endl;  // Between 45 and 49 is Grade D
+        cout << "Your Grade is D." << endl;  
     } 
     else if (marks >= 50 && marks <= 59) {
-        cout << "Your Grade is C." << endl;  // Between 50 and 59 is Grade C
+        cout << "Your Grade is C." << endl;  
     } 
     else if (marks >= 60 && marks <= 69) {
-        cout << "Your Grade is B." << endl;  // Between 60 and 69 is Grade B
+        cout << "Your Grade is B." << endl;  
     } 
     else if (marks >= 70) {
-        cout << "Your Grade is A." << endl;  // 70 and above is Grade A
-    }
+        cout << "Your Grade is A." << endl;
+    }  
     else {
-        cout << "Invalid marks entered." << endl;  // Handles unexpected cases
-    }
-
+        cout << "Invalid marks." << endl;  
     return 0;
+    }
 }
