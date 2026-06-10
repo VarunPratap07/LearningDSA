82fffe2c68c03c2e0a2ef4e1a2b5cde9b29adb78
VarunPratap07 <varunpratapsinghshishodia@gmail.com>
2026-06-10 11:22:41 +0530

    Commit.

---
+++ TheBasics/Input_Output/Simple_Input.cpp
@@ -0,0 +1,8 @@
+//Simple integar input and display.
+#include <iostream>
+int main(){
+    int x ;
+    std::cout << "Please enter the value of X = " ;
+    std::cin >> x ;
+    std::cout << "The value of X is "<< x << "." ;
+}
---
+++ TheBasics/Input_Output/Text_Output.cpp
@@ -0,0 +1,5 @@
+//Simple Text Output.
+#include <iostream>
+int main(){
+    std::cout<<"Hello World!";
+}
