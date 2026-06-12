b20f59b271b968be56fc5b566d28742c0734504f
VarunPratap07 <varunpratapsinghshishodia@gmail.com>
2026-06-12 19:38:32 +0530

    Commit.

---
+++ 02_Basic_Maths/Nth_Prime_Number.cpp
@@ -0,0 +1,29 @@
+//Function to find the N'th prime number.
+#include <iostream>
+using namespace std ;
+int Nth_Prime(int n){
+    int nth=0 , prime , i=0 ;
+    while(nth!=n){
+        if(i==0||i==1){
+            i++ ;
+        }
+        int factor=0 ;
+        for(int j=1 ; j<=i ; j++){
+            if(i%j==0){
+                factor++;
+            }
+        }
+        if(factor==2){
+            nth++ ;
+            prime=i ;
+        }
+        i++ ;    
+    }
+    return prime ; 
+}
+int main(){
+    int n ;
+    cout<<"Please enter what prime you want-" ;
+    cin>>n ;
+    cout<<"The "<<n<<" prime number "<<"is "<<Nth_Prime(n) ;
+}
