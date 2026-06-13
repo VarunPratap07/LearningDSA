cfe845abc5e8baed06852dfafb924b9ed29543df
VarunPratap07 <varunpratapsinghshishodia@gmail.com>
2026-06-13 10:28:02 +0530

    Commit.

---
+++ 03_Pattern_Printing/Pattern_13.cpp
@@ -0,0 +1,28 @@
+/*
+Program to print the following pattern-
+
+1      1
+12    21
+123  321
+12344321
+
+*/
+#include <iostream>
+using namespace std ;
+int main(){
+    int n ;
+    cout<<"Please enter the height of the pattern -" ;
+    cin>>n ;
+    for(int i=1 ; i<=n ; i++){
+        for(int j=1 ; j<=i ; j++){
+            cout<<j ;
+        }
+        for(int j=1 ; j<=2*n-2*i ; j++){
+            cout<<" ";
+        }
+        for(int j=i ; j>=1 ; j--){
+            cout<<j ;
+        }
+        cout<<endl ;
+    }
+}
